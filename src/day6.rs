805ec899ccafd818a053eccc87e7c7d7d2c531b0
D.J. Rideout <djrideoutgit@proton.me>
2025-12-06 14:47:41 +0000

    Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -25,3 +25,38 @@ pub fn solve_part1(input: &str) -> u64 {
         });
     sum
 }
+
+#[aoc(day6, part2)]
+pub fn solve_part2(input: &str) -> u64 {
+    let op_re = Regex::new(r"[*+]\s*").unwrap();
+    let mut sum = 0;
+    op_re
+        .find_iter(input.lines().last().unwrap())
+        .for_each(|op| {
+            let op_char = op.as_str().chars().nth(0).unwrap();
+            let mut nums = vec![0; op.end() - op.start()];
+            let mut offsets = vec![0; op.end() - op.start()];
+            for (i, l) in input.lines().rev().skip(1).enumerate() {
+                let digits = &l[op.start()..op.end()];
+                for (j, c) in digits.chars().enumerate() {
+                    if c != ' ' {
+                        let digit: u64 = c.to_string().parse().unwrap();
+                        nums[j] += digit * 10_u64.pow(i as u32 - offsets[j]);
+                    } else {
+                        offsets[j] += 1;
+                    }
+                }
+            }
+            let result = nums.into_iter().reduce(|acc, el| {
+                if el != 0 {
+                    return match op_char {
+                        '+' => acc + el,
+                        _ => acc * el
+                    };
+                }
+                acc
+            });
+            sum += result.unwrap();
+        });
+    sum
+}
