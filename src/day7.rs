42beef3b15b3cc2971ef4ed2bb72a2dac3f0be6a
D.J. Rideout <djrideoutgit@proton.me>
2025-12-07 15:18:51 +0000

    Day 7 refactor to use recursive solution for part 1 instead of iterative

--- src/day7.rs
+++ src/day7.rs
@@ -1,27 +1,6 @@
 use std::collections::HashMap;
 
-#[aoc(day7, part1)]
-pub fn solve_part1(input: &str) -> usize {
-    let start = input.find("S").unwrap();
-    let mut beams = vec![start];
-    let mut count = 0;
-    for l in input.lines().skip(2) {
-        let splitters = l.match_indices("^");
-        for s in splitters {
-            let check_hit = beams.iter().enumerate().find(|(_, b)| **b == s.0);
-            if let Some((i, hit)) = check_hit {
-                count += 1;
-                beams.append(&mut vec![*hit - 1, *hit + 1]);
-                beams.remove(i);
-            }
-        }
-        beams.sort();
-        beams.dedup();
-    }
-    count
-}
-
-fn split(input: &str, line_num: usize, pos: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+fn split(input: &str, line_num: usize, pos: usize, split_count: &mut usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
     let key = (line_num, pos);
     if cache.contains_key(&key) {
         return *cache.get(&key).unwrap();
@@ -31,16 +10,25 @@ fn split(input: &str, line_num: usize, pos: usize, cache: &mut HashMap<(usize, u
         return 1;
     }
     if l.unwrap().chars().nth(pos).unwrap() != '^' {
-        return split(input, line_num + 2, pos, cache);
+        return split(input, line_num + 2, pos, split_count, cache);
     }
-    let timelines = split(input, line_num + 2, pos - 1, cache)
-        + split(input, line_num + 2, pos + 1, cache);
+    let timelines = split(input, line_num + 2, pos - 1, split_count, cache)
+        + split(input, line_num + 2, pos + 1, split_count, cache);
+    *split_count += 1;
     cache.insert(key, timelines);
     timelines
+}
+
+#[aoc(day7, part1)]
+pub fn solve_part1(input: &str) -> usize {
+    let start = input.find("S").unwrap();
+    let mut split_count = 0;
+    split(input, 2, start, &mut split_count, &mut HashMap::new());
+    split_count
 }
 
 #[aoc(day7, part2)]
 pub fn solve_part2(input: &str) -> usize {
     let start = input.find("S").unwrap();
-    split(input, 2, start, &mut HashMap::new())
+    split(input, 2, start, &mut 0, &mut HashMap::new())
 }
