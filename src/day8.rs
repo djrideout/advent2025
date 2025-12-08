b07ced10f78b9f7f1d0f64335bc99abba48a41f4
D.J. Rideout <djrideoutgit@proton.me>
2025-12-08 19:29:29 +0000

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -6,7 +6,7 @@ type Pairs = BinaryHeap<Pair>;
 type Input = (Vec<Point>, Pairs);
 type Set = HashSet<Point>;
 
-#[derive(Eq, PartialEq, Clone, Debug)]
+#[derive(Eq, PartialEq, Clone)]
 pub struct Pair {
     a: Point,
     b: Point,
@@ -51,8 +51,7 @@ pub fn input_generator(input: &str) -> Input {
     (points, pairs)
 }
 
-#[aoc(day8, part1)]
-pub fn solve_part1((points, pairs): &Input) -> usize {
+fn do_joins((points, pairs): &Input) -> (usize, usize) {
     let mut pairs = pairs.clone();
     let mut sets: Vec<Set> = vec![];
     for point in points {
@@ -60,10 +59,14 @@ pub fn solve_part1((points, pairs): &Input) -> usize {
         set.insert(*point);
         sets.push(set);
     }
+    let mut sets_at_thousand: Option<Vec<Set>> = None;
+    let mut last_joined_pair: Option<Pair> = None;
     let mut count = 0;
     while let Some(pair) = pairs.pop() {
         if count == 1000 {
-            break;
+            let mut sets_clone = sets.clone();
+            sets_clone.sort_by(|a, b| b.len().cmp(&a.len()));
+            sets_at_thousand = Some(sets_clone);
         }
         count += 1;
         let a_set_idx = sets.iter().position(|s| s.contains(&pair.a)).unwrap();
@@ -71,12 +74,27 @@ pub fn solve_part1((points, pairs): &Input) -> usize {
         if a_set_idx == b_set_idx {
             continue;
         }
+        last_joined_pair = Some(pair);
         let b_points: Vec<Point> = sets[b_set_idx].iter().cloned().collect();
         for p in b_points {
             sets[b_set_idx].remove(&p);
             sets[a_set_idx].insert(p);
         }
     }
-    sets.sort_by(|a, b| b.len().cmp(&a.len()));
-    sets[0].len() * sets[1].len() * sets[2].len()
+    let sets_at_thousand = sets_at_thousand.unwrap();
+    let last_joined_pair = last_joined_pair.unwrap();
+    (
+        sets_at_thousand[0].len() * sets_at_thousand[1].len() * sets_at_thousand[2].len(),
+        last_joined_pair.a.0 * last_joined_pair.b.0
+    )
+}
+
+#[aoc(day8, part1)]
+pub fn solve_part1(input: &Input) -> usize {
+    do_joins(input).0
+}
+
+#[aoc(day8, part2)]
+pub fn solve_part2(input: &Input) -> usize {
+    do_joins(input).1
 }
