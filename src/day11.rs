c3286c63f5dda05a69ac34bc49c227822758bd68
D.J. Rideout <djrideoutgit@proton.me>
2025-12-11 17:07:15 +0000

    Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -19,35 +19,46 @@ pub fn input_generator(input: &str) -> Input {
     graph
 }
 
-fn paths(node: String, path: Vec<&str>, graph: &Input, cache: &mut HashMap<String, usize>) -> usize {
+fn paths(node: String, path: Vec<&str>, graph: &Input, cache: &mut HashMap<String, (usize, usize, usize, usize)>) -> (usize, usize, usize, usize) {
     if cache.contains_key(&node) {
         return *cache.get(&node).unwrap();
     }
-    let mut count = 0;
+    let mut counts = (0, 0, 0, 0);
     let children = graph.get(&node).unwrap();
     for child in children {
         let mut next_path = path.clone();
         next_path.push(child);
-        if path[0] == "svr" && child == "out" {
-            if path.contains(&"fft") && path.contains(&"dac") {
-                return 1;
-            }
-            return 0;
-        } else if child == "out" {
-            return 1;
+        if child == "out" {
+            return (1, 0, 0, 0);
         }
-        count += paths(child.clone(), next_path, graph, cache);
+        let result = paths(child.clone(), next_path, graph, cache);
+        counts.0 += result.0;
+        counts.1 += result.1;
+        counts.2 += result.2;
+        counts.3 += result.3;
+        if node == "dac" && result.2 > 0 {
+            counts.3 += result.2;
+        }
+        if node == "fft" && result.1 > 0 {
+            counts.3 += result.1;
+        }
     }
-    cache.insert(node.clone(), count);
-    count
+    if node == "dac" {
+        counts.1 += counts.0;
+    }
+    if node == "fft" {
+        counts.2 += counts.0;
+    }
+    cache.insert(node.clone(), counts);
+    counts
 }
 
 #[aoc(day11, part1)]
 pub fn solve_part1(input: &Input) -> usize {
-    paths("you".to_string(), vec!["you"], input, &mut HashMap::new())
+    paths("you".to_string(), vec!["you"], input, &mut HashMap::new()).0
 }
 
 #[aoc(day11, part2)]
 pub fn solve_part2(input: &Input) -> usize {
-    paths("svr".to_string(), vec!["svr"], input, &mut HashMap::new())
+    paths("svr".to_string(), vec!["svr"], input, &mut HashMap::new()).3
 }
